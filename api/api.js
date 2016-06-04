669b2a2b5eb4ec2f3896a24331019dcd05a0a01b
Grant Holcombe <grantalanholcombe@gmail.com>
2016-06-03 18:13:49 -0700

    half way thru setting up the scraper,  pausing for gf

--- api/api.js
+++ api/api.js
@@ -1,8 +1,37 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
 var express = require('express');
 var api = express();
 
 // set port
 var port = process.env.PORT || 8080;
+
+// scrape function
+api.get('/scrape', function(req, res){
+  // set url for scrape
+  url = "http://" + "mscmiramar" + '.clubspeedtiming.com/sp_center/RacerHistory.aspx?CustID=' + "1170635";
+
+  // scrape the url
+  request(url, function(err, res, html){
+    if(!err){
+      var $ = cheerio.load(html);
+      var json = {
+        dates: []
+      }
+
+      $('#Table1 table .Normal td:nth-child(2)').each(function(){
+        var date = $(this).text().replace(' ', '');
+        json.dates.push(date);
+      });
+
+
+      console.log(json);
+    } else {
+      console.log();
+    }
+  });
+});
 
 // url route setup
 var router = express.Router();
@@ -13,15 +42,14 @@ router.get('/', function(req, res) {
   });
 });
 
-
 router.route('/fetch-times/:t/:r')
   .get(function(req, res) {
       var trackName = req.params.t;
       var racerId = req.params.r;
 
       res.json({
-        trackName: trackName,
-        racerId: racerId
+        trackName: "mscmiramar",
+        racerId: "1170635"
       });
   });
 
