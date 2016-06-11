a66f4c40fb2674a35cdcccab4ac968df594a81b9
Grant Holcombe <grantalanholcombe@gmail.com>
2016-06-10 17:29:18 -0700

    mobile fixes and ux tweaks implemented

--- js/app/app.js
+++ js/app/app.js
@@ -22,6 +22,12 @@ angular.module("holcomberacing", ['ngMaterial', 'chart.js', 'httpServices'])
           var response = response.data;
           // set times and dates from response
           var dates = response.dates.reverse();
+          // clean each date they look long
+            for(var i = 0; i < dates.length; i++){
+              var removeTime = dates[i].substring( 0, dates[i].split('').indexOf(' '));
+              var trimYear = removeTime.substring(0, removeTime.length - 4) + removeTime.substring(removeTime.length - 2, removeTime.length)
+              dates[i] = trimYear;
+            }
           var laps = response.times.reverse();
           // get lap avg from laps arr
         	var avg = laps.reduce(function(p,c,i){return p+(c-p)/(i+1)},0).toFixed(3);
@@ -48,7 +54,7 @@ angular.module("holcomberacing", ['ngMaterial', 'chart.js', 'httpServices'])
               scaleFontColor: "#ffffff"
           }
 
-          $scope.newPr = (parseFloat(laps[laps.length - 1]) === $scope.pr);
+          $scope.newPr = (parseFloat(laps[laps.length - 1]).toFixed(3) === $scope.pr);
 
 
         },
